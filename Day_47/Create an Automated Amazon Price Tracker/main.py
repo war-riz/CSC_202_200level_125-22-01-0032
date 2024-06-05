2b4c7ff37381210e171de8effabe3c7d8026604e
war-riz <kehindewaris54@gmail.com>
2024-06-05 07:37:57 +0100

    creating an automated amazon price tracker

---
+++ Day_47/Create an Automated Amazon Price Tracker/main.py
@@ -0,0 +1,43 @@
+# Using beautifulsoup to scrap amazon website for pressure-cooking pot
+
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+# print(soup.prettify())
+
+price = soup.find(class_="a-offscreen").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+
+
+# Email alert when price below preset value
+import smtplib
+
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login("wareezakanmu249@gail.com", "??zir_raw??")
+        connection.sendmail(
+            from_addr="wareezakanmu249@gail.com",
+            to_addrs="wareezakanmu249@gail.com",
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
